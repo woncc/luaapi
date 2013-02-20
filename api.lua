f7b3f37fae595440b2fd464f47eb2284675ff6f1
millken <millken@gmail.com>
2013-02-20 15:14:03 +0800

    add mysql support

--- api.lua
+++ api.lua
@@ -1,15 +1,15 @@
 module("api",package.seeall)
 
 local JSON = require("cjson")
-
+local mysql = require "mysql"
 function hello(req, resp, name)
     if req.method=='GET' then
         -- resp:writeln('Host: ' .. req.host)
         -- resp:writeln('Hello, ' .. ngx.unescape_uri(name))
         -- resp:writeln('name, ' .. req.uri_args['name'])
          resp.headers['Content-Type'] = 'application/json'
-        ngx.print("in demo2\n")
-        ngx.print(JSON.encode(req.uri_args))
+        ff = mysql.Mysql:query("select id,username,age from users where id=2")
+        ngx.print(ff)  
     elseif req.method=='POST' then
         -- resp:writeln('POST to Host: ' .. req.host)
         req:read_body()
---
+++ mysql.lua
@@ -0,0 +1,44 @@
+#!/usr/bin/env lua
+
+module('mysql',package.seeall)
+local JSON = require("cjson")
+local mysql = require "resty.mysql"
+local db = nil
+Mysql={}
+
+function Mysql:new()
+        db,err = mysql:new()
+        if not db then
+                ngx.say("failed to instantiate mysql: ",err)
+                return
+        end
+
+        db:set_timeout(1000)
+
+        local ok,err,errno,sqlstate = db:connect{
+                host = "127.0.0.1",
+                port = 3306,
+                database = "test",
+                user = "root",
+                password = "yd-db-2012",
+                max_package_size = 1024
+        }
+        if not ok then
+                ngx.say("failed to connect: ", err, ": ", errno, " ", sqlstate)
+                return
+        end
+
+        db:query("set names utf8")
+        return db
+end
+
+
+function Mysql:query(sql)
+    Mysql:new()
+    res,err,errno,sqlstate = db:query(sql)
+    if not res then
+            ngx.say("bad result: ", err, ": ", errno, ": ", sqlstate, ".")
+            return
+    end
+    return JSON.encode(res)
+end
